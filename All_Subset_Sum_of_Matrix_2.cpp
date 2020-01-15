4b80ec171a63de77f6e0b2f6b9e84cfa4ec984cf
Atul Sharma <53703302+atul852798@users.noreply.github.com>
2020-01-15 17:40:52 +0530

    all subset sum of matrix in O(n^2)

---
+++ All_Subset_Sum_of_Matrix_2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define R 4
+#define C 5
+using namespace std;
+
+void sub_sum(int a[][C])
+{
+    int sum=0;
+    for(int i=0;i<R;++i)
+        for(int j=0;j<C;++j)
+         {
+            int x=(i+1)*(j+1);
+            int y=(R-i)*(C-j);
+            sum+=a[i][j]*(i+1)*x*y;        //final sum is a[][] * the no. of submatrix the cell will be present
+         }
+
+    cout<<sum;
+}
+
+int main()
+{
+    int a[][C]={{1,1,1,1,1},
+                {1,1,1,1,1},
+                {1,1,1,1,1},
+                {1,1,1,1,1}
+                };
+
+    sub_sum(a);
+}

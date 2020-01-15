b4c9de00ce9d709c88736df1d1bd4f6a2aec7b47
Atul Sharma <53703302+atul852798@users.noreply.github.com>
2020-01-15 10:02:56 +0530

    All Subset sum of 2D matrix in O(n^6)

---
+++ All_Subset_Sum_of_Matrix_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int sub_sum(int a[][2])
+{
+    int sum=0,summ;
+    for(int li=0;li<2;++li)
+      {
+        for(int lj=0;lj<2;++lj)                   //this & above loop set top left value
+         {
+            for(int bi=0;bi<2;++bi)
+            {
+                 for(int bj=0;bj<2;++bj)          //this & above loop set bottom right value
+                 {
+                     for(int i=li;i<=bi;++i)       //now we run the loop from top left value to bottom right value
+                     {
+                          for(int j=lj;j<=bj;++j)
+                          {
+                               sum+=a[i][j];        //calculate all subset sum of matrix
+                          }
+                     }
+
+                 }
+            }
+         }
+     }
+   return sum;
+}
+
+int main()
+{
+    int a[][2]={{1,1},
+                {1,1}};
+
+    cout<<sub_sum(a);
+}

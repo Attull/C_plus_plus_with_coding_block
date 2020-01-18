1b661b1d7f3c40f2b1d89e2de90fccfec36f6722
Atul Sharma <53703302+atul852798@users.noreply.github.com>
2020-01-18 10:15:29 +0530

    Prifix sum of 2D matrix

---
+++ prifix_sum_of_2D_matrix.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#define R 4
+#define C 5
+using namespace std;
+
+int pre[R][C];
+
+void prefix_sum(int a[][C],int p,int q,int x,int y)
+{
+    for(int i=0;i<R;++i)
+    {
+        for(int j=0;j<C;++j)
+        {
+          if(i>0 && j>0)
+          {
+              pre[i][j]=a[i][j]+pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1];
+          }
+          else if(i==0 && j>0)                           //for first row
+          {
+              pre[i][j]=a[i][j]+pre[i][j-1];
+          }
+          else if(i>0 && j==0)                           //for first column
+          {
+              pre[i][j]=a[i][j]+pre[i-1][j];
+          }
+          else
+          {
+              pre[i][j]=a[i][j];
+          }
+        }
+    }
+    for(int i=0;i<R;++i)
+    {
+        for(int j=0;j<C;++j)
+        {
+            cout<<pre[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+int main()
+{
+    int a[][C]={{1,1,1,1,1},
+                {1,1,1,1,1},
+                {1,1,1,1,1},
+                {1,1,1,1,1}};
+
+    cout<<"prefix matrix \n";
+    prefix_sum(a,1,1,3,4);        //pass array and top left index & bottom right index whose subset has to be calculated
+}

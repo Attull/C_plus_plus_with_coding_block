35f0287e33ae832b97d69182942da049a9594b7f
Atul Sharma <53703302+Attull@users.noreply.github.com>
2020-01-21 10:35:09 +0530

    lower bound of element in 1D array
    lowe bound/first occurrence

---
+++ first_occurence_of_ele_1D_array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int lower_bd(int arr[],int n,int ele)
+{
+
+    int beg=0,last=n-1;
+    int mid,ans=-1;
+
+    while(beg<=last)
+    {
+        mid=(beg+last)/2;
+        if(ele==arr[mid])
+        {
+            ans=mid;
+            last=mid-1;
+        }
+        else if(ele<arr[mid])
+        {
+            last=mid-1;
+        }
+        else
+            beg=mid+1;
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[]={2,3,3,7,3,12,76};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int ele;
+    sort(arr,arr+n);
+
+    cout<<"enter element to find its first occurrence/lower bound \n";
+    cin>>ele;
+    int ans=lower_bd(arr,n,ele);
+
+    cout<<"first occurrence of given element is : "<<ans;
+
+    return 0;
+}

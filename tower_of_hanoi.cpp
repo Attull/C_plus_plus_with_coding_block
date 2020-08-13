ace97bf7d6233db0819cbbdb91f4612b3ba157a9
Atul Sharma <53703302+Attull@users.noreply.github.com>
2020-08-13 10:50:54 +0530

    Update tower_of_hanoi.cpp

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void toi(int n,char src,char dest,char helper)
+void toi(int n,char src,char helper,char dest)
 {
     //base case
     if(n==0)
@@ -11,14 +11,14 @@ void toi(int n,char src,char dest,char helper)
 
     //recursive case
     //first case n-1 disks move from src to helper
-    toi(n-1,src,helper,dest);
+    toi(n-1,src,dest,helper);
     cout<<"move "<<n<<" disk from "<<src<<" to "<<dest<<endl;
-    toi(n-1,helper,dest,src);
+    toi(n-1,helper,src,dest);
 
 }
 int main()
 {
     int n;
     cin>>n;
-    toi(n,'A','C','B');
+    toi(n,'A','B','C');
 }
